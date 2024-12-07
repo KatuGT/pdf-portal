d302f33e81943492596bb03fd62e26c0cdc48fc4
jimena katu <93katu@gmail.com>
2024-12-07 12:21:22 -0300

    first commit

---
+++ src/components/PDF.tsx
@@ -0,0 +1,432 @@
+"use client";
+import { FormValues } from "@/utils";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  Image,
+} from "@react-pdf/renderer";
+
+
+const styles = StyleSheet.create({
+  page: {
+    marginTop: 80,
+  },
+  mainContainer: {
+    marginHorizontal: "auto",
+    width: "90%",
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    columnGap: 10,
+  },
+  headerImage: {
+    height: "auto",
+    width: "45%",
+    objectFit: "contain",
+  },
+  headerData: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "black",
+    borderStyle: "solid",
+  },
+  textSize: {
+    fontSize: 9,
+    fontWeight: 500,
+  },
+  textContainer: {
+    paddingHorizontal: 5,
+    paddingVertical: 10,
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    justifyContent: "space-around",
+  },
+  datoHeader: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+  },
+  middelSection: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "black",
+    borderStyle: "solid",
+    marginTop: 20,
+  },
+  detalleSection: {
+    borderWidth: 1,
+    borderColor: "black",
+    borderStyle: "solid",
+    marginTop: 10,
+    minHeight: "55%",
+    position: "relative",
+  },
+  detallePadding: {
+    paddingHorizontal: 5,
+    paddingVertical: 2,
+    fontSize: 8,
+  },
+  linea: {
+    position: "absolute",
+    height: "100%",
+    width: 1,
+    backgroundColor: "black",
+    top: 0,
+    zIndex: "-1",
+  },
+  border: {
+    borderWidth: 1,
+    borderColor: "black",
+    borderStyle: "solid",
+  },
+});
+
+const PDF = ({
+  itemsList,
+  comprobante = "",
+  presupuesto,
+  fecha,
+  cliente,
+  domicilio,
+  vendedor,
+  contacto,
+  IVA,
+  expediente,
+  condVenta,
+}: FormValues) => {
+  const [year, month, day] = fecha.split("-");
+
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.mainContainer}>
+          <View style={styles.header}>
+            <Image src="./logo-portal.png" style={styles.headerImage} />
+            <View style={styles.headerData}>
+              <View
+                style={[
+                  styles.textContainer,
+                  {
+                    borderRightColor: "black",
+                    borderRightStyle: "solid",
+                    borderRightWidth: 1,
+                    flex: 1,
+                  },
+                ]}
+              >
+                <Text style={styles.textSize}>C.U.I.T.: 30-71816831-3</Text>
+                <Text style={styles.textSize}>Ing.Bruto: 380620</Text>
+                <Text style={styles.textSize}>Inicio de Act.: 01/08/2023</Text>
+              </View>
+              <View
+                style={[
+                  {
+                    borderRightColor: "black",
+                    borderRightStyle: "solid",
+                    borderRightWidth: 1,
+                    flex: 0.7,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    gap: 5,
+                  },
+                ]}
+              >
+                <Image
+                  src="./x.png"
+                  style={{
+                    height: 30,
+                    width: 30,
+                    objectFit: "contain",
+                    borderWidth: 1,
+                    borderColor: "black",
+                    borderStyle: "solid",
+                    padding: 5,
+                  }}
+                />
+                <Text
+                  style={[
+                    {
+                      textAlign: "center",
+                      marginHorizontal: "auto",
+                      fontSize: 6,
+                    },
+                  ]}
+                >
+                  DOCUMENTO NO VALIDO COMO FACTURA
+                </Text>
+              </View>
+              <View
+                style={[styles.textContainer, { flex: 1, textAlign: "right" }]}
+              >
+                <View style={styles.datoHeader}>
+                  <Text style={[styles.textSize, { flex: 1 }]}>
+                    Comprobante:
+                  </Text>
+                  <Text
+                    style={[
+                      styles.textSize,
+                      { flex: 0.8, textAlign: "left", marginLeft: 3 },
+                    ]}
+                  >
+                    {comprobante}
+                  </Text>
+                </View>
+
+                <View style={styles.datoHeader}>
+                  <Text style={[styles.textSize, { flex: 1 }]}>
+                    Presupuesto Nº:
+                  </Text>
+                  <Text
+                    style={[
+                      styles.textSize,
+                      { flex: 0.8, textAlign: "left", marginLeft: 3 },
+                    ]}
+                  >
+                    {presupuesto}
+                  </Text>
+                </View>
+                <View style={styles.datoHeader}>
+                  <Text style={[styles.textSize, { flex: 1 }]}>Fecha:</Text>
+                  <Text
+                    style={[
+                      styles.textSize,
+                      { flex: 0.8, textAlign: "left", marginLeft: 3 },
+                    ]}
+                  >
+                    {`${day}/${month}/${year}`}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={styles.middelSection}>
+            <View
+              style={{
+                paddingVertical: 5,
+                paddingHorizontal: 10,
+                borderRightColor: "black",
+                borderRightStyle: "solid",
+                borderRightWidth: 1,
+                flex: 2,
+                display: "flex",
+                flexDirection: "column",
+                gap: 3,
+              }}
+            >
+              <View style={styles.datoHeader}>
+                <Text style={[styles.textSize, { textAlign: "right" }]}>
+                  Señor/a:
+                </Text>
+                <Text
+                  style={[
+                    styles.textSize,
+                    { flex: 1, textAlign: "left", marginLeft: 3 },
+                  ]}
+                >
+                  {cliente}
+                </Text>
+              </View>
+              <View style={styles.datoHeader}>
+                <Text style={[styles.textSize, { textAlign: "right" }]}>
+                  Domicilio:
+                </Text>
+                <Text
+                  style={[
+                    styles.textSize,
+                    { flex: 1, textAlign: "left", marginLeft: 3 },
+                  ]}
+                >
+                  {domicilio}
+                </Text>
+              </View>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                }}
+              >
+                <View style={styles.datoHeader}>
+                  <Text style={[styles.textSize, {}]}>Vendedor:</Text>
+                  <Text
+                    style={[
+                      styles.textSize,
+                      { flex: 0.8, textAlign: "left", marginLeft: 3 },
+                    ]}
+                  >
+                    {vendedor}
+                  </Text>
+                </View>
+                <View style={styles.datoHeader}>
+                  <Text style={[styles.textSize, { flex: 1 }]}>Contacto:</Text>
+                  <Text
+                    style={[
+                      styles.textSize,
+                      { flex: 0.8, textAlign: "left", marginLeft: 3 },
+                    ]}
+                  >
+                    {contacto}
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                display: "flex",
+                flexDirection: "column",
+                gap: 3,
+                paddingVertical: 5,
+                paddingHorizontal: 10,
+              }}
+            >
+              <View style={styles.datoHeader}>
+                <Text
+                  style={[styles.textSize, { flex: 1, textAlign: "right" }]}
+                >
+                  I.V.A.:
+                </Text>
+                <Text
+                  style={[
+                    styles.textSize,
+                    { flex: 2, textAlign: "left", marginLeft: 3 },
+                  ]}
+                >
+                  {IVA}
+                </Text>
+              </View>
+              <View style={styles.datoHeader}>
+                <Text
+                  style={[styles.textSize, { flex: 1, textAlign: "right" }]}
+                >
+                  EXPTE. Nº :
+                </Text>
+                <Text
+                  style={[
+                    styles.textSize,
+                    { flex: 2, textAlign: "left", marginLeft: 3 },
+                  ]}
+                >
+                  {expediente}
+                </Text>
+              </View>
+              <View style={styles.datoHeader}>
+                <Text
+                  style={[styles.textSize, { flex: 1, textAlign: "right" }]}
+                >
+                  Cond. Venta:
+                </Text>
+                <Text
+                  style={[
+                    styles.textSize,
+                    { flex: 2, textAlign: "left", marginLeft: 3 },
+                  ]}
+                >
+                  {condVenta}
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.detalleSection}>
+            <Text
+              style={{
+                backgroundColor: "black",
+                textAlign: "center",
+                paddingVertical: 2,
+                color: "white",
+                fontSize: 9,
+              }}
+            >
+              DETALLE
+            </Text>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                borderBottom: "1px solid black",
+              }}
+            >
+              <View style={[styles.detallePadding, { flex: 1 }]}>
+                <Text>Item</Text>
+              </View>
+              <View style={[styles.detallePadding, { flex: 15 }]}>
+                <Text>Descripción</Text>
+              </View>
+              <View style={[styles.detallePadding, { flex: 2 }]}>
+                <Text>Cantidad</Text>
+              </View>
+              <View style={[styles.detallePadding, { flex: 4 }]}>
+                <Text>Precio</Text>
+              </View>
+              <View style={[styles.detallePadding, { flex: 4 }]}>
+                <Text>Subtotal</Text>
+              </View>
+            </View>
+            {itemsList?.length > 0 &&
+              itemsList?.map((item, index) => (
+                <View
+                  key={index}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    position: "relative",
+                    borderBottom: "1px solid black",
+                  }}
+                >
+                  <View
+                    style={[
+                      styles.detallePadding,
+                      { flex: 1, textAlign: "center" },
+                    ]}
+                  >
+                    <Text>{index + 1}</Text>
+                  </View>
+                  <View style={[styles.detallePadding, { flex: 15 }]}>
+                    <Text>{item.descripcion}</Text>
+                  </View>
+                  <View
+                    style={[
+                      styles.detallePadding,
+                      { flex: 2, textAlign: "center" },
+                    ]}
+                  >
+                    <Text>{item.cantidad}</Text>
+                  </View>
+                  <View style={[styles.detallePadding, { flex: 4 }]}>
+                    <Text>${item?.precio?.toLocaleString("es-ES")}</Text>
+                  </View>
+                  <View style={[styles.detallePadding, { flex: 4 }]}>
+                    <Text>
+                      $
+                      {item?.precio && item?.cantidad
+                        ? (item?.precio * item?.cantidad).toLocaleString(
+                            "es-ES"
+                          )
+                        : 0}
+                    </Text>
+                  </View>
+                </View>
+              ))}
+            <View style={[styles.linea, { left: 29 }]} />
+            <View style={[styles.linea, { left: 315 }]} />
+            <View style={[styles.linea, { left: 363 }]} />
+            <View style={[styles.linea, { left: 445 }]} />
+          </View>
+        </View>
+      </Page>
+    </Document>
+  );
+};
+
+export default PDF;
---
+++ src/pages/index.tsx
@@ -0,0 +1,342 @@
+import { Button } from "@nextui-org/button";
+import { PDFViewer } from "@react-pdf/renderer";
+import { Control, useFieldArray, useForm, useWatch } from "react-hook-form";
+import { useEffect, useState } from "react";
+
+import DefaultLayout from "@/layouts/default";
+import { subtitle } from "@/components/primitives";
+import Logo from "../../public/logo-portal.png";
+import PDF from "@/components/PDF";
+import { defaultDataCliente, defaultDataVendedor, FormValues } from "@/utils";
+
+function getTotal(payload: FormValues["itemsList"]) {
+  let total = 0;
+
+  for (const item of payload) {
+    if (item.precio && item.cantidad) {
+      total = total + +item.precio * +item.cantidad;
+    }
+  }
+
+  return total.toLocaleString("es-ES");
+}
+
+function TotalAmout({ control }: { control: Control<FormValues> }) {
+  const cartValues = useWatch({
+    control,
+    name: "itemsList",
+  });
+
+  return <span>Monto total: ${getTotal(cartValues)}</span>;
+}
+
+export default function IndexPage() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // Mes empieza en 0
+  const day = String(today.getDate()).padStart(2, "0");
+  const { register, watch, control, reset, getValues, setValue } =
+    useForm<FormValues>({
+      defaultValues: {
+        itemsList: [
+          {
+            descripcion: "",
+            cantidad: 1,
+            precio: null,
+          },
+        ],
+        comprobante: "",
+        presupuesto: "",
+        fecha: `${year}-${month}-${day}`,
+        cliente: "",
+        domicilio: "",
+        vendedor: "",
+        contacto: "",
+        IVA: "EXENTO",
+        expediente: "",
+        condVenta: "CTA CTE",
+      },
+    });
+
+  const { fields, append, remove } = useFieldArray({
+    name: "itemsList",
+    control,
+    rules: {
+      required: "Agrega un item almenos.",
+    },
+  });
+
+  const datos = watch();
+
+  // console.log(datos);
+
+  const cliente = watch("cliente");
+  const vendedor = watch("vendedor");
+
+  useEffect(() => {
+    if (cliente && defaultDataCliente[cliente]) {
+      setValue("domicilio", defaultDataCliente[cliente].domicilio);
+    }
+  }, [cliente, reset, getValues]);
+
+  useEffect(() => {
+    if (vendedor && defaultDataVendedor[vendedor]) {
+      setValue("contacto", defaultDataVendedor[vendedor].contacto);
+    }
+  }, [vendedor, reset, getValues]);
+
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col justify-center gap-4 py-8 md:py-10">
+        <h2 className={subtitle({ class: "mt-4" })}>Datos fijos:</h2>
+        <div className="border rounded  w-full flex gap-3 flex-wrap">
+          <figure className="w-fit bg-white ">
+            <img
+              alt="logo portal informatica"
+              className="max-w-sm w-full"
+              src={Logo}
+            />
+          </figure>
+          <article className="px-5 py-5">
+            <p>C.U.I.T.: 30-71816831-3</p>
+            <p>Ing.Bruto: 380620</p>
+            <p>Inicio de Act.: 01/08/2023</p>
+          </article>
+        </div>
+        <div className="flex w-full justify-between">
+          <h2 className={subtitle({ class: "mt-4" })}>Datos editables:</h2>
+          <Button color="warning" onClick={() => reset()}>
+            Borrar datos
+          </Button>
+        </div>
+
+        <form className="border rounded  w-full  p-5 ">
+          <div className="flex gap-6 flex-wrap">
+            <div className="flex flex-col gap-6">
+              <label className="flex flex-col gap-1">
+                <span>Comprobante: </span>
+                <input
+                  className="py-1 px-2 border border-gray-500 rounded"
+                  {...register(`comprobante`, {
+                    required: true,
+                  })}
+                />
+              </label>
+
+              <label className="flex flex-col gap-1">
+                <span>Presupuesto Nº:</span>
+                <input
+                  className="py-1 px-2 border border-gray-500 rounded"
+                  type="number"
+                  {...register(`presupuesto`, {
+                    required: true,
+                  })}
+                />
+              </label>
+              <label className="flex flex-col gap-1">
+                <span>Fecha:</span>
+                <input
+                  className="py-1 px-2 border border-gray-500 rounded"
+                  type="date"
+                  {...register(`fecha`, {
+                    required: true,
+                  })}
+                />
+              </label>
+            </div>
+            <div className="flex flex-col gap-6">
+              <label className="flex flex-col gap-1">
+                <span>Señor/a: </span>
+                <input
+                  className="py-1 px-2 border border-gray-500 rounded"
+                  list="clientes"
+                  {...register(`cliente`, {
+                    required: true,
+                  })}
+                />
+                <datalist id="clientes">
+                  <option value="DIRECCION DE MATERIALES Y CONSTRUCCIONES ESCOLARES" />
+                  <option value="Policia" />
+                </datalist>
+              </label>
+              <label className="flex flex-col gap-1">
+                <span>Domicilio:</span>
+                <input
+                  {...register(`domicilio`, {
+                    required: true,
+                  })}
+                  className="py-1 px-2 border border-gray-500 rounded"
+                />
+              </label>
+              <label className="flex flex-col gap-1">
+                <span>Vendedor: </span>
+                <input
+                  className="py-1 px-2 border border-gray-500 rounded"
+                  list="vendedores"
+                  {...register(`vendedor`, {
+                    required: true,
+                  })}
+                />
+                <datalist id="vendedores">
+                  <option value="PALACIO EZEQUIEL" />
+                  <option value="DIP RAMIRO" />
+                </datalist>
+              </label>
+              <label className="flex flex-col gap-1">
+                <span>Contacto:</span>
+                <input
+                  {...register(`contacto`, {
+                    required: true,
+                  })}
+                  className="py-1 px-2 border border-gray-500 rounded"
+                />
+              </label>
+            </div>
+            <div className="flex flex-col gap-6">
+              <label className="flex flex-col gap-1">
+                <span>I.V.A.: </span>
+                <input
+                  className="py-1 px-2 border border-gray-500 rounded"
+                  defaultValue={"EXENTO"}
+                  {...register(`IVA`, {
+                    required: true,
+                  })}
+                />
+              </label>
+              <label className="flex flex-col gap-1">
+                <span>EXPTE. Nº:</span>
+                <input
+                  {...register(`expediente`, {
+                    required: true,
+                  })}
+                  className="py-1 px-2 border border-gray-500 rounded"
+                />
+              </label>
+              <label className="flex flex-col gap-1">
+                <span>Cond. Venta: </span>
+                <input
+                  className="py-1 px-2 border border-gray-500 rounded"
+                  defaultValue={"CTA CTE"}
+                  {...register(`condVenta`, {
+                    required: true,
+                  })}
+                />
+              </label>
+            </div>
+          </div>
+          <hr className="my-5" />
+          <div className="flex flex-col gap-6 flex-wrap">
+            <h2 className={subtitle()}>Detalle:</h2>
+
+            {fields.map((field, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex gap-6 flex-wrap border-b-neutral-600 border-b-1 pb-6 "
+                >
+                  <label className="flex flex-col gap-1 flex-1">
+                    <span>Descripción: </span>
+                    <input
+                      className="py-1 px-2 border border-gray-500 rounded"
+                      {...register(`itemsList.${index}.descripcion`, {
+                        required: true,
+                      })}
+                    />
+                  </label>
+                  <label className="flex flex-col gap-1 shrink-0">
+                    <span>Cantidad: </span>
+                    <input
+                      className="py-1 px-2 border w-20 border-gray-500 rounded"
+                      {...register(`itemsList.${index}.cantidad`, {
+                        required: true,
+                      })}
+                      type="number"
+                    />
+                  </label>
+                  <label className="flex flex-col gap-1 shrink-0">
+                    <span>Precio: </span>
+                    <input
+                      className="py-1 px-2 border w-40 border-gray-500 rounded"
+                      min={0}
+                      placeholder="0"
+                      type="number"
+                      {...register(`itemsList.${index}.precio`, {
+                        required: true,
+                      })}
+                    />
+                  </label>
+                  <Button
+                    className="mt-auto"
+                    color="danger"
+                    type="button"
+                    onClick={() => remove(index)}
+                  >
+                    Borrar
+                  </Button>
+                </div>
+              );
+            })}
+            <TotalAmout control={control} />
+            <Button
+              type="button"
+              onClick={() => {
+                append({
+                  descripcion: "",
+                  cantidad: 1,
+                  precio: 0,
+                });
+              }}
+            >
+              Agregar item
+            </Button>
+          </div>
+        </form>
+        {loaded ? (
+          <PDFViewer height={1200}>
+            <PDF
+              IVA={datos.IVA}
+              cliente={datos.cliente}
+              comprobante={datos.comprobante}
+              condVenta={datos.condVenta}
+              contacto={datos.contacto}
+              domicilio={datos.domicilio}
+              expediente={datos.expediente}
+              fecha={datos.fecha}
+              itemsList={datos.itemsList}
+              presupuesto={datos.presupuesto}
+              vendedor={datos.vendedor}
+            />
+          </PDFViewer>
+        ) : (
+          ""
+        )}
+        {/* <PDFDownloadLink}
+        document={
+          <PDF
+            IVA={datos.IVA}
+            cliente={datos.cliente}
+            comprobante={datos.comprobante}
+            condVenta={datos.condVenta}
+            contacto={datos.contacto}
+            domicilio={datos.domicilio}
+            expediente={datos.expediente}
+            fecha={datos.fecha}
+            itemsList={datos.itemsList}
+            presupuesto={datos.presupuesto}
+            vendedor={datos.vendedor}
+          />
+        }
+        fileName="dsfdsf"
+      >
+        Descargar
+      </PDFDownloadLink> */}
+      </section>
+    </DefaultLayout>
+  );
+}
